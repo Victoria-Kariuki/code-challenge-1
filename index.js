c359b079083ca8ca34a4a20aaaa05f00f5c48441
victoria <kariukivictoria428@gmail.com>
2024-03-27 07:51:54 +0300

    challenge1

---
+++ index.js
@@ -0,0 +1,25 @@
+//input between 0 and 100
+function studentGrade(marks){
+    if (marks>= 0 && marks<= 100){
+    if(marks>=79)
+    return "A";
+
+    else if(marks>=60)
+     return "B"
+
+     else if(marks>=50)
+      return "C"
+
+      else if(marks>=40)
+      return "D"
+
+      else if (marks<=40 && marks >=0)
+      return "E"
+
+      else(
+        alert //should be greater than 0 and less than 100
+        //positive numbers only
+      )
+    }
+}      
+console.log (50)
